a4380e7878487bf241d56e659007dd6d0961c82f
ShedrackGodson <hedrickgodson23@gmail.com>
2021-02-22 14:56:58 +0300

    social logins

--- devs/urls.py
+++ devs/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from devs.views import (
-    index, settings, password
+    index, settings, password, change_password
 )
 
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path("", index, name="home"),
     path('settings/', settings, name='settings'),
     path('settings/password/', password, name='password'),
+    path('settings/password/<int:dev_id>/change-password/', change_password, name='change_password'),
 ]
 
--- devs/views.py
+++ devs/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import AdminPasswordChangeForm, PasswordChangeForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth import authenticate, update_session_auth_hash
 from social_django.models import UserSocialAuth
 from django.shortcuts import redirect, render
 from django.contrib import messages
@@ -64,3 +64,25 @@ def password(request):
         form = PasswordForm(request.user)
     return render(request, 'devs/password.html', {'form': form})
 
+
+@login_required
+def change_password(request, dev_id):
+    dev = Dev.objects.get(id=dev_id)
+    current_password = request.POST.get("current_password")
+    new_password = request.POST.get("new_password")
+    new_password2 = request.POST.get("new_password2")
+    try:
+        status = authenticate(request, username=dev.username,
+                              password=current_password)
+        if status:
+            if new_password == new_password2:
+                dev.set_password(new_password)
+                dev.save()
+                messages.success(request, "Password successfully changed.")
+            else:
+                messages.error(request, "Passwords not matching.")
+        else:
+            messages.error(request, "Invalid credentials.")
+    except Exception as e:
+        print(e)
+    return redirect("settings")
